ba77ec7a4b873047de905784b06d2b2450898274
Raul Dipeas <rauldipeas@disroot.org>
2025-11-21 04:06:42 -0300

    +primeiro-commit

---
+++ .vscode/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+git diff --name-only HEAD |
+  grep '\.pacscript$' |
+  while read -r f; do
+    ./scripts/srcinfo.sh write "$f"
+  done
+
+./scripts/srcinfo.sh build all
+
+auto_push() {
+  added=()
+  deleted=()
+  modified=()
+  while read -r status file; do
+    base="$(basename "$file")"
+    name="${base%.*}"
+    case "$status" in
+    A*) added+=("+$name") ;;
+    D*) deleted+=("-$name") ;;
+    M*) modified+=("^$name") ;;
+    esac
+  done < <(git status --porcelain)
+  msg=""
+  [ ${#added[@]} -gt 0 ] && msg+=$'\n'$(printf "%s\n" "${added[@]}")
+  [ ${#deleted[@]} -gt 0 ] && msg+=$'\n'$(printf "%s\n" "${deleted[@]}")
+  [ ${#modified[@]} -gt 0 ] && msg+=$'\n'$(printf "%s\n" "${modified[@]}")
+  git add .
+  [ -z "$msg" ] && exit 0
+  git commit -m "$msg"
+  git push -u origin main
+}
+auto_push
